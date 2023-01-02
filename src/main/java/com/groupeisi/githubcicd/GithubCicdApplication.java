e2d49091e02d817d010449b1b50baeff5cdb76ed
msgassama <gassamasanoussy15@gmail.com>
2023-01-02 23:10:34 +0000

    added endpoint

--- src/main/java/com/groupeisi/githubcicd/GithubCicdApplication.java
+++ src/main/java/com/groupeisi/githubcicd/GithubCicdApplication.java
@@ -2,12 +2,14 @@ package com.groupeisi.githubcicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class GithubCicdApplication {
 
+	@GetMapping("/")
 	public String welcome(){
 		return "Welcome to spring lab github-actions";
 	}
